75e66b5b6f33d1481fa4088179bf1ab317b14b76
Gitea <mking@arikel.net>
2018-11-22 15:15:00 -0500

    add a helper

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
 export { Pi3Model, ISnapModel } from './snapcraft/models';
+
+import { ISnapModel, Pi3Model } from './snapcraft/models';
+
+export const Pi3Defaults: ISnapModel = {
+    type: 'model',
+    authorityId: 'test-authority-id',
+    brandId: 'test-brand-id',
+    series: '16',
+    model: 'test-model',
+    architecture: 'ignored',
+    gadget: 'ignored',
+    kernel: 'ignored',
+    timestamp: new Date().toISOString(),
+}
+
+function createPi3Model(obj: object): Pi3Model {
+    let snapModel: ISnapModel;
+    snapModel = obj as ISnapModel;
+    return new Pi3Model(snapModel);
+}
+
+export { createPi3Model as CreatePi3 };
